a7dc062df660a0076c0e5c60d80fa79350d921af
Shone-Kahuria <shonekahuria@gmail.com>
2025-10-01 13:36:19 +0300

    Cleanup: Remove obsolete files and update PHPMailer references

--- includes/email.php
+++ includes/email.php
@@ -4,9 +4,9 @@
  * Provides email functionality for the mentoring website
  */
 
-require_once __DIR__ . '/../vendor/phpmailer/src/PHPMailer.php';
-require_once __DIR__ . '/../vendor/phpmailer/src/SMTP.php';
-require_once __DIR__ . '/../vendor/phpmailer/src/Exception.php';
+require_once __DIR__ . '/../Plugins/PHPMailer/src/PHPMailer.php';
+require_once __DIR__ . '/../Plugins/PHPMailer/src/SMTP.php';
+require_once __DIR__ . '/../Plugins/PHPMailer/src/Exception.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
